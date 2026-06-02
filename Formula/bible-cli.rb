539296cd29dc659a60e3e00500f425f946dbbe3e
Migen Karriqi <mkarriqi@gmail.com>
2026-06-01 22:48:10 -0500

    bible-cli 0.4.0

--- Formula/bible-cli.rb
+++ Formula/bible-cli.rb
@@ -1,8 +1,8 @@
 class BibleCli < Formula
   desc "Fast, playful Bible CLI built in Rust"
   homepage "https://github.com/migkapa/bible-cli"
-  url "https://github.com/migkapa/bible-cli/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "e1c778139c0e2d7264add5dba38f4c307a13bd9a6116ca0beb81820efa12738a"
+  url "https://github.com/migkapa/bible-cli/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "f1485de2a03e4f49d65038ebfdbe49532b919fac4abc307411eac7ec9511b1f7"
   license any_of: ["MIT", "Apache-2.0"]
 
   depends_on "rust" => :build
